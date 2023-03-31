2dc09d573de199539d6c31ade05420f0d89e3ff8
yurrrrri <yu_ri_son@naver.com>
2023-03-31 13:44:07 +0900

    RsData 도입

--- src/main/java/com/ll/gramgram/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.boundedContext.member.service;
 
+import com.ll.gramgram.base.rsData.RsData;
 import com.ll.gramgram.boundedContext.member.entity.Member;
 import com.ll.gramgram.boundedContext.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +23,18 @@ public class MemberService {
     }
 
     @Transactional
-    public void join(String username, String password) {
+    public RsData<Member> join(String username, String password) {
+        if(findByUsername(username).isPresent()) {
+            return RsData.of("F-1", "이미 사용 중인 아이디입니다.");
+        }
+
         Member member = Member
                 .builder()
                 .username(username)
                 .password(passwordEncoder.encode(password))
                 .build();
         memberRepository.save(member);
+
+        return RsData.of("S-1", "회원가입이 완료되었습니다.", member);
     }
 }
