5a3d52efd21a90212c2230451c501f3218d217fc
yurrrrri <yu_ri_son@naver.com>
2023-03-31 15:55:47 +0900

    인스타 계정 등록

--- src/test/java/com/ll/gramgram/boundedContext/member/controller/InstaMemberControllerTest.java
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/InstaMemberControllerTest.java
@@ -1,18 +1,23 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
 import com.ll.gramgram.boundedContext.instaMember.controller.InstaMemberController;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -26,6 +31,9 @@ public class InstaMemberControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private InstaMemberService instaMemberService;
 
     @Test
     @DisplayName("인스타 회원 정보 입력 폼")
@@ -67,6 +75,31 @@ public class InstaMemberControllerTest {
                 .andExpect(handler().methodName("showConnect"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrlPattern("**/member/login**"));
+    }
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("인스타 정보 입력 폼 처리")
+    @WithUserDetails("user1")
+    void t003() throws Exception {
+        //when
+        ResultActions resultActions = mvc
+                .perform(post("/instaMember/connect")
+                        .with(csrf())
+                        .param("username", "abc123")
+                        .param("gender", "W"))
+                .andDo(print()); // 확인용
+        //then
+        resultActions
+                .andExpect(handler().handlerType(InstaMemberController.class))
+                .andExpect(handler().methodName("connect"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("/pop**"));
+
+        InstaMember instaMember = instaMemberService.findByUsername("abc123").orElse(null);
+
+        assertThat(instaMember).isNotNull();
+
     }
 
 }
